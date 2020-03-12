1a2367d142c0c846cd78e6591599d6db9c7d6eb4
soju6jan <cybersol@naver.com>
2020-03-13 00:46:10 +0900

    1

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from plugin import blueprint, menu, plugin_load, plugin_unload, process_telegram_data
+from plugin import blueprint, menu, plugin_load, plugin_unload, process_telegram_data, plugin_info
 
